9a47338a6f537da8c24d8d650be5c747ee648a7b
Jon Friskics <jonfriskics@gmail.com>
2019-04-24 00:22:14 -0400

    module1-solution

---
+++ src/components/BookList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+import BookItem from './BookItem';
+import BookForm from './BookForm';
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      title: 'All Books'
+    };
+  },
+  components: {
+    BookItem,
+    BookForm,
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    },
+  },
+};
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
