6cc3998deb0c9ab4a12c7a4a37cd2253c5ecb285
Jon Friskics <jonfriskics@gmail.com>
2019-04-24 00:38:18 -0400

    module4-solution

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,12 +2,13 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-      <li v-for='book in books'>{{book.title}}:{{book.author}}</li>
+      <book-item v-for='book in books' :book='book'></book-item>
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from './BookItem';
 export default {
   name: 'BookList',
   data() {
@@ -19,6 +20,9 @@ export default {
         {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
       ]
     };
+  },
+  components: {
+    BookItem
   }
 };
 </script>
