4b53563a24e0d03188976b1c0bfb9296f3b3cd0a
sherry <zsherry0803@gmail.com>
2020-01-07 14:39:04 -0500

    m2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,22 +1,39 @@
 <template>
-    <div>
-        <h1>{{title}}</h1>
-    </div>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "BookList",
-    data(){
-        return {
-            title: "All Books"
-        }
-    }
-}
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+      { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+      { title: "American Gods", author: "Neil Gaiman" },
+      { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+    ]
+    };
+  }
+};
 </script>
 
 <style>
-h1, h2{
-    font-weight: normal,
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
