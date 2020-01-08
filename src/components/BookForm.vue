0b0d0cf72ce322f296a89351d26d32fe8eec1e1f
sherry <zsherry0803@gmail.com>
2020-01-08 10:54:20 -0500

    final

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,23 +1,25 @@
 <template>
-  <form class="" action="#" method="post" @submit.prevent="bookSubmit(bookTitle, bookAuthor)">
-    <input v-model="bookTitle" type="text" name="title" value='' placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value='' placeholder="Book Author">
-    <button type="submit" name="button" >Add Book</button>
-</form>
+  <form class action="#" method="post" @submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+    <input v-model="bookTitle" type="text" name="title" value placeholder="Book Title" />
+    <input v-model="bookAuthor" type="text" name="author" value placeholder="Book Author" />
+    <button type="submit" name="button">Add Book</button>
+  </form>
 </template>
 
 <script>
 export default {
   name: "BookForm",
-  props: ['books'],
+  props: ["books"],
   data() {
-      bookTitle:'';
-      bookAuthor: '';
+    return {
+      bookTitle: "",
+      bookAuthor: ""
+    };
   },
   methods: {
-      bookSubmit(bookTitle, bookAuthor) {
-          this.$emit('addBook', bookTitle, bookAuthor);
-      }
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit("addBook", bookTitle, bookAuthor);
+    }
   }
 };
 </script>
